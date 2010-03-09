60591e0c8ac6251e4b5df6ccfe17e6c7e53a8855
kvnvlmr <kvnvlmr@70e90580-223b-11df-abce-ff734413008c>
2010-03-09 18:29:45 +0000

    Created LevelObject

---
+++ trunk/Paint the Game/src/LevelObject.java
@@ -0,0 +1,7 @@
+/*
+ * Kevin Voellmer
+ * LevelObject is the superclass for Coin, Path, Stickman, and Abstract Obstacles (Spikedwall, Doctor, Wall).
+ */
+public class LevelObject {
+	
+}//end class
