ac8275c816a45097b275c384c815bdd1ffc9aaa0
wendymao10 <wendymao10@70e90580-223b-11df-abce-ff734413008c>
2010-04-09 16:55:39 +0000

    Creates GUI that displays JFrame on HowToPlayV2; however HowToPlayV2 is a dud! Just use the original Applet HowToPlay class.

---
+++ trunk/src/HowToPlayTester.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class HowToPlayTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window=new HowToPlayV2();
+		window.setVisible(true);
+
+	}
+
+}
