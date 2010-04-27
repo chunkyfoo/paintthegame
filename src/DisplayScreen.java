5bff48bb81e4e2f9ace415ebebc9608262b11883
ekostreski@gmail.com <ekostreski@gmail.com@70e90580-223b-11df-abce-ff734413008c>
2010-04-27 17:43:17 +0000

    Example Level Object

--- src/DisplayScreen.java
+++ src/DisplayScreen.java
@@ -15,19 +15,25 @@ import javax.swing.JPanel;
 
 
 public class DisplayScreen extends JPanel implements Runnable{
-	
+
 	Level level;
 	long start;
 	long total;
 	long pass;
+	long pauseTime = (long) 0.0;
+	long systemPauseTime = (long) 0.0;
 	long frames = 0;
 	String sPass = "";
 	public boolean notQuit = true;
-	
+	public boolean pause = false;
 	public DisplayScreen(){
 		level = new Level(new File("testLevel"));
 	}
-	
+
+	private long getTime(){
+		return System.currentTimeMillis() - pauseTime;
+	}
+
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Iterator<LevelObject> levelIterator = level.levelObjects.iterator();
@@ -39,32 +45,42 @@ public class DisplayScreen extends JPanel implements Runnable{
 		g2.setColor(Color.GREEN);
 		g2.setFont(g2.getFont().deriveFont(24F));
 		g2.drawString("Average FPS = " + String.valueOf(frames/(total/1000.0)), 20, 20);
-		g2.drawString(sPass, 20, 50);
-		g2.drawString(String.valueOf(total/1000.0),20,70);
+		g2.drawString(sPass, 20, 50);//lag
+		g2.drawString(String.valueOf(total/1000.0),20,70);//time
 		//System.out.println(this.getWidth());
 	}
 
-	
+
 	public void run() {
 		this.setVisible(true);
-		start = System.currentTimeMillis();
-		total = System.currentTimeMillis() - start;
-		pass = System.currentTimeMillis() - total;
+		start = getTime();
+		total = getTime() - start;
+		pass = getTime() - total;
 		long passt = pass;
 		long wPass = 0;//time to increment
 		while(notQuit){
-			total = System.currentTimeMillis() - start;
-			this.repaint();
-			pass = wPass + System.currentTimeMillis() - passt;
-			//System.out.println(pass);
-			wPass = System.currentTimeMillis();
-			this.increment();
-			wPass = System.currentTimeMillis() - wPass;
-			sPass = String.valueOf(pass);
-			pass = 0;
-			passt = System.currentTimeMillis();
+			if(pause){
+				if(systemPauseTime == 0l)
+					systemPauseTime = getTime();
+			}
+			else{
+				if(systemPauseTime > 0l){
+					pauseTime += getTime() - systemPauseTime;
+					systemPauseTime = 0L;
+				}
+				total = getTime() - start;
+				this.repaint();
+				pass = wPass + getTime() - passt;
+				//System.out.println(pass);
+				wPass = getTime();
+				this.increment();
+				wPass = getTime() - wPass;
+				sPass = String.valueOf(pass);
+				pass = 0;
+				passt = getTime();
+			}
 		}//end while
-		
+
 	}//end method run
 
 	private void increment() {
