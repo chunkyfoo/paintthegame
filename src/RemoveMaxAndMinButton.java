310c8d7a9e3541bea71e603ee530cac56bc27c66
ekostreski@gmail.com <ekostreski@gmail.com@70e90580-223b-11df-abce-ff734413008c>
2010-04-30 18:30:34 +0000

    Example Level Object

---
+++ src/RemoveMaxAndMinButton.java
@@ -0,0 +1,16 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class RemoveMaxAndMinButton extends JDialog{
+	  public RemoveMaxAndMinButton(JFrame frame, String str){
+	    super(frame,str);
+	    addWindowListener(new WindowAdapter(){
+	            public void windowClosing(WindowEvent evt){
+	        System.exit(0);
+	            }
+	        });
+	  }
+}
