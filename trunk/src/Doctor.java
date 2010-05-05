507b56aac2b6610c4975964b69bfd04214605935
kvnvlmr <kvnvlmr@70e90580-223b-11df-abce-ff734413008c>
2010-05-05 17:59:57 +0000

    added movment

--- trunk/src/Doctor.java
+++ trunk/src/Doctor.java
@@ -3,18 +3,39 @@ import javax.swing.ImageIcon;
 
 
 public class Doctor extends LevelObject{
+	double radians;
+	double distance;
+	double interval;
+	boolean horizontal;
 	public Doctor(){
 		super(new ImageIcon("Instructions.png").getImage());
+		radians =  0;
+		distance = 0;
+		interval = 0;
+		horizontal = false;
 	}//end default constructor
-	
-	public Doctor(int x, int y){
+
+	public Doctor(int x, int y, double distance, double interval, boolean horizontal){
 		super(x, y, new ImageIcon("Instructions.png").getImage());
+		radians = 0;
+		this.distance = distance;
+		this.interval = interval;
+		this.horizontal = horizontal;
 	}//end default constructor
+
+	public void draw(Graphics2D g2) {
+		if(horizontal)
+		g2.drawImage(img, x + (int)(distance*Math.sin(radians)), y, null);
+		else
+		g2.drawImage(img, x, y + (int)(distance*Math.sin(radians)), null);
+	}//end draw
 	
 	@Override
 	public void increment(long total, long pass) {
-		// TODO Auto-generated method stub
-		
+		radians += 2 * Math.PI * (double)pass / interval;
+		while(radians >= 2 *Math.PI){
+			radians -= 2 * Math.PI;
+		}//end while
 	}//end increment
-	
+
 }//end class
